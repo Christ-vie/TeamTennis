d2cf9f80d1abf09a046e153a6ac3a114c7054f57
christmiller <christmiller@gmail.com>
2021-01-06 13:52:19 +0100

    second

--- tennis/serializers.py
+++ tennis/serializers.py
@@ -1,22 +1,22 @@
 from rest_framework import serializers
 from .models import *
-class AbonnementSerializer(serializers.ModelSerializer):
+class AbonnementSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Abonnement
         fields = '__all__'
-class PersonnelSerializer(serializers.ModelSerializer):
+class PersonnelSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Personnel
         fields = '__all__'
-class CompteSerializer(serializers.ModelSerializer):
+class CompteSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Compte
         fields = '__all__'
-class EntrepriseSerializer(serializers.ModelSerializer):
+class EntrepriseSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Entreprise
         fields = '__all__'
-class FactureSerializer(serializers.ModelSerializer):
+class FactureSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Facture
         fields = '__all__'
--- tennis/urls.py
+++ tennis/urls.py
@@ -5,6 +5,9 @@ from tennis import views
 router = routers.DefaultRouter()
 router.register('personnels', views.PersonnelView)
 router.register('abonnements', views.AbonnementView)
+router.register('entreprises', views.EntrepriseView)
+router.register('factures', views.FactureView)
+router.register('comptes', views.CompteView)
 
 urlpatterns = [
     path('', include(router.urls))
